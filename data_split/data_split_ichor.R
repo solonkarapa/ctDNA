8c2d0bf732651c7348810f6ef86182a6cdb4d665
solon <karapanagiotissolon@gmail.com>
2021-05-10 16:34:50 +0100

    start split code

---
+++ data_split/data_split_ichor.R
@@ -0,0 +1,6 @@
+#############################################################################
+# splits data into train and test sets based on ichor CNA 
+# observation where time_ichor > time_CT are deleted - these should be 2
+#############################################################################
+
+library(dplyr)
