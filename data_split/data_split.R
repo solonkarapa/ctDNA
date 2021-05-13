3ca9c5860d35c0810bb717f96d6f2f2d77a5cc16
solon <karapanagiotissolon@gmail.com>
2021-05-13 11:52:50 +0100

    data split code added

---
+++ data_split/data_split.R
@@ -0,0 +1,73 @@
+
+#############################################################################
+# splits data into train and test sets based on CT scans
+# test set: includes last observation per patient (if > 1 observation per patient)
+# train set: the remaining observations
+#############################################################################
+
+################ load packages
+library(dplyr)
+
+################ load data
+load("~/Box/PhD/Code/ctDNA/updated/RECIST_Treatments.Rdata") # for mac
+
+################ 
+# transformations and select cols
+data_RECIST_time <- RECIST_treatments %>% 
+    mutate(time = as.numeric(time/52.14), # time in years
+           Treatment_duration = Treatment_duration/52.14) %>% # duration in years
+    select(Patient.ID, Date, Progression, ER.status, Her2.status, Treatment_new_final, Treatment_duration, time)
+
+################ 
+#select patients for dynamic predictions
+#patients_dyn_pred <- c("DT158", "DT171", "DT213", "DT232", "DT343") # first version
+patients_dyn_pred <- c("DT158", "DT213", "DT343") # second version
+
+data_ind_pat_CT <- data_RECIST_time %>% 
+    filter(Patient.ID %in% patients_dyn_pred) 
+
+data_excluded_ind_pat <- data_RECIST_time %>% 
+    filter(!Patient.ID %in% patients_dyn_pred)
+
+################ 
+# keep patients with > 1 ctDNA measurment             
+patients_keep <- data_excluded_ind_pat %>% 
+    group_by(Patient.ID) %>% 
+    summarise(measur_per_patient = n()) %>% 
+    filter(measur_per_patient > 1)
+
+# filter based on max Date per Patient.ID and >1 ctDNA measurment
+df_max_Date <- data_excluded_ind_pat %>% 
+    filter(Patient.ID %in% patients_keep$Patient.ID) %>% 
+    group_by(Patient.ID) %>% 
+    slice_max(Date)
+
+#df_max_Date %>% ungroup() %>% summarise(n())
+
+# return all rows from x with a match in y
+data_test_CT <- data_excluded_ind_pat %>% 
+    semi_join(df_max_Date, by = c("Date", "Patient.ID"), copy = T) 
+
+# return all rows from x without a match in y
+data_train_CT <- data_excluded_ind_pat %>% 
+    anti_join(data_test_CT, by = c("Date", "Patient.ID"), copy = T) 
+
+#setwd("~/Box/PhD/Code/ctDNA/updated/data_split")
+#save(data_train_CT, file = "data_train_CT.Rdata")
+#save(data_test_CT, file = "data_test_CT.Rdata")
+#save(data_ind_pat_CT, file = "data_dynamic_pred_CT.Rdata")
+
+################
+# checks
+#dim(data_excluded_ind_pat)[1] == dim(data_train_CT)[1] + dim(data_test_CT)[1]
+
+# all should be one
+#data_test_CT %>% summarise(n()) %>% arrange(desc(`n()`))
+
+# calculates the difference in measurements between full and train sets - should be 1 
+#data_train_CT %>% 
+#    filter(Patient.ID %in% patients_keep$Patient.ID) %>% 
+#    summarise(measur_per_patient = n()) %>%
+#    mutate(diff = patients_keep$measur_per_patient - measur_per_patient) %>%
+#    summarise(range(diff))
+
--- data_split/data_split_ichor.R
+++ data_split/data_split_ichor.R
@@ -1,6 +1,35 @@
 #############################################################################
-# splits data into train and test sets based on ichor CNA 
+# splits data into train and test sets based on ichorCNA 
 # observation where time_ichor > time_CT are deleted - these should be 2
 #############################################################################
 
 library(dplyr)
+
+##################################################################   
+########################## IchorCNA ##############################
+################################################################## 
+# load ichor dataset
+load("~/Box/PhD/Code/ctDNA/updated/ichorCNA_Treatments.Rdata") # updated dataset
+
+# time unit conversion
+# https://www.datasciencemadesimple.com/get-difference-between-two-dates-in-r-by-days-weeks-months-and-years-r-2/
+# time in years - time/52.14
+data_ichorCNA_time <- ichorCNA_Treatments %>% 
+    mutate(time = as.numeric(time)) %>%
+    mutate(time = time/52.14, # time in years
+           Treatment_duration = Treatment_duration/52.14) %>% # time in years
+    select(Patient.ID, Date, ichorCNA_tr, ER.status, Her2.status, time, Treatment_new_final, Treatment_duration)
+
+##################################################################   
+############################# CT ################################# 
+##################################################################
+# load CT dataset
+load("~/Box/PhD/Code/ctDNA/updated/RECIST_treatments.Rdata") # for mac
+
+# transformations and select cols
+data_RECIST_time <- RECIST_treatments %>% 
+    mutate(time = as.numeric(time/52.14), # time in years
+           Treatment_duration = Treatment_duration/52.14) %>% # time in years
+    select(Patient.ID, Date, Progression, ER.status, Her2.status, Treatment_new_final, time, Treatment_duration)
+
+
