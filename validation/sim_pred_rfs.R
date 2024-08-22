ccba409f8b17c84e78354a8b96b6136bb16e84be
solon <karapanagiotissolon@gmail.com>
2024-08-22 10:27:00 +0100

    validation sampling

--- validation/sim_pred_rfs.R
+++ validation/sim_pred_rfs.R
@@ -1,4 +1,11 @@
 
+# get array element number
+task_id_string <- Sys.getenv("SLURM_ARRAY_TASK_ID") 
+task_id <- as.numeric(task_id_string)
+#task_id = 2
+
+########################################################################
+
 library(dplyr)
 library(brms)
 library(tidyr)
@@ -8,22 +15,21 @@ library(purrr)
 #################################### Step 1 ############################
 ########################################################################
 # load data
-path_data <- "/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/validation/"
+path_data <- "/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/validation/" # mac
+#path_data <- "/home/sk921/rds/rds-mrc-bsu/sk921/PhD/Code/ctDNA/updated/validation/" # HPC
+
 load(paste0(path_data, "validation_data.Rdata"))
 
-
-id <- 840
-
-subject <- id #unique(df_train_ichor_ind$Patient.ID)
+subject <- unique(df_RECIST_Ant$Patient.ID)
 
 data_new_stage1 <- df_ichor_Ant %>% 
-    filter(Patient.ID == subject) %>% 
+    filter(Patient.ID == subject[task_id]) %>% 
     arrange(Date.ichor)
 
 print(data_new_stage1)
 
 data_new_stage2 <- df_RECIST_Ant %>% 
-    filter(Patient.ID == subject) %>% 
+    filter(Patient.ID == subject[task_id]) %>% 
     arrange(Date)
 
 print(data_new_stage2)
@@ -60,7 +66,7 @@ df_new_preds_1_split <- df_new_preds_1 %>% group_by(Date) %>% group_split()
 ########################################################################
 # load stage 1 model
 load("/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/models/model_1st_stage.Rdata") # for mac
-#load("~/HPC/PhD/Code/ctDNA/updated/models/model_1st_stage.Rdata") # for HPC
+#load("/home/sk921/rds/rds-mrc-bsu/sk921/PhD/Code/ctDNA/updated/models/model_1st_stage.Rdata") # for HPC
 
 model_first_stage <- fit1_ichor
 
@@ -95,7 +101,7 @@ for(g in 1:length(df_new_preds_1_split)){
     print(g)
     
     datagrid <- expand.grid(i = as.numeric(1:iters),
-                            index_patient = id,
+                            index_patient = subject[task_id],
                             timepoint = unique(data_new_stage1$time))
     
     res <- datagrid %>%
@@ -136,8 +142,8 @@ str(df_new_preds)
 #################################### Step 5 ############################
 ########################################################################
 # load stage 2 model
-load("/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/models/model_2nd_stage_ichor.Rdata") # for mac
-#load("~/HPC/PhD/Code/ctDNA/updated/models/model_2nd_stage_ichor.Rdata") # for HPC
+#load("/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/models/model_2nd_stage_ichor.Rdata") # for mac
+load("/home/sk921/rds/rds-mrc-bsu/sk921/PhD/Code/ctDNA/updated/models/model_2nd_stage_ichor.Rdata") # HPC
 
 # calculate posterior predictive distribution  
 post_pred_ctDNA <- posterior_epred(fit2_CT_ichor, 
@@ -152,5 +158,17 @@ post_pred_avg <- colMeans(post_pred_ctDNA)
 df_new_preds_final <- cbind(df_new_preds, pred_prob = post_pred_avg)
 
 df_new_preds_final
+
+output_file <- paste0("output_pred_", task_id, ".Rdata")
+
+#### 
+setwd("/Users/solon/Cloud-Drive/Projects/ctDNA_original/ctDNA/validation/predictions/output/") # mac
+#setwd("/rds/user/sk921/hpc-work/PhD/ctDNA/updated/predictions/output_ctDNA_project_treatment_2") # HPC uni
+
+save(data_new_stage1,
+     df_new_preds_final,
+     post_pred_ctDNA, 
+     file = output_file) 
+
 
 
